77f67e9169a0c5ee583be1bc3a62d59a25620092
Jina Song <jinasong@unc.edu>
2026-04-19 11:27:40 -0400

    Initial Commit

---
+++ finalProjectMusicApp/finalProjectMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectMusicAppApp.swift
+//  finalProjectMusicApp
+//
+//  Created by Jina Song on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
